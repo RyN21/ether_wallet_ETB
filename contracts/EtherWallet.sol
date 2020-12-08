d221ea0a144366b33ced37cc0db7066d44a10a54
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-08 06:40:56 -0700

    Add constructor owner

--- contracts/EtherWallet.sol
+++ contracts/EtherWallet.sol
@@ -1,11 +1,17 @@
 pragma solidity ^0.5.0;
 
 contract EtherWallet {
+  address public owner;
+
+  constructor(address _owner) public {
+    owner = _owner;
+  }
+
   function deposit() payable public {
     // no logic needed
   }
 
   function send(address to, uint amount) public {
-    to.transfer(amount);
+      to.transfer(amount);
   }
 };
